7b9255e09c0f8efe08efcbb3500342543cd47c5a
Joel Hoisko <joel.hoisko@gmail.com>
2021-05-27 00:20:55 +0200

    Add next and mute keys

--- keyboards/lily58/keymaps/joelhoisko/config.h
+++ keyboards/lily58/keymaps/joelhoisko/config.h
@@ -34,7 +34,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 100
 
 #undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
+// #define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_LIMIT_VAL 120
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
